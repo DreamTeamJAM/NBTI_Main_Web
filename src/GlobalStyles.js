736e70242d10c275ea14a7015ddb2e10bc7120de
esmerado <esmerado.vela.javier@gmail.com>
2022-03-29 14:41:23 +0100

    Navbar Language

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,5 +1,10 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
+/** Global Styles */
 export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 
-`
