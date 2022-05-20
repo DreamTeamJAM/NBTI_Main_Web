25ed7f2397817be07fe1aae9f2e3deb702f42808
Antonio <tonidiez17@gmail.com>
2022-05-20 15:53:15 +0100

    closes Crear pagina de log In #78

---
+++ src/Pages/Register/register.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container } from "GlobalStyles";
+
+function register() {
+  return (
+    <Container>
+    <form>
+    <u>Welcome</u>
+    
+    </form>
+    </Container>
+  )
+}
+
+export default register
