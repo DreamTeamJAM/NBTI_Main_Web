2fd850140774ed92e0b2888af4fc15eca1277c2e
meskion <manufdh@hotmail.com>
2022-06-07 16:13:22 +0100

    Updated Api method for student

--- src/Components/Navbar/ProfileIcon/index.js
+++ src/Components/Navbar/ProfileIcon/index.js
@@ -1,20 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { UserButton } from "./styles";
-import { getStudentById } from "services/api/studentApi";
+import { getStudentByUserId } from "services/api/studentApi";
 import { downloadFile } from "services/api/fileApi";
+import AuthService from "services/auth/auth.service";
+
 import {Img} from "./styles";
 
 export default function ProfileIcon({ onClick }) {
   const [img, setImg] = useState();
-  const [StudentValues, setStudentValues] = useState("");
+  
   useEffect(() => {
-    getStudentById(21).then((res) => {
-      setStudentValues(res.data);
-      downloadFile(res.data.photoId).then((file) => {
-        setImg(file.data);
-      });
-    
+    AuthService.getCurrentUser().then((user) => {
+      getStudentByUserId(user.id).then((res) => {
+        downloadFile(res.data.photoId).then((file) => {
+          setImg(file.data);
+        });
     });
+   });
   }, []);
 
   return <UserButton onClick={onClick} ><Img src={`data:image/jpeg;base64, ${img}`}></Img></UserButton>;
--- src/services/api/studentApi.js
+++ src/services/api/studentApi.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 import authHeader from '../auth/auth-header';
 
-const BASE_HOST = 'https://34vznuxt9f.execute-api.eu-west-1.amazonaws.com/beta/student'
+const BASE_HOST = 'https://34vznuxt9f.execute-api.eu-west-1.amazonaws.com/beta'
 
 
 export const getAllStudents = async () => {
 
-    const response = await axios.get(`${BASE_HOST}`, {
+    const response = await axios.get(`${BASE_HOST}/student`, {
         headers: authHeader()
     });
     return response.data
@@ -14,15 +14,23 @@ export const getAllStudents = async () => {
 
 export const getStudentById = async ( id) => {
 console.log(authHeader())
-    const response = await axios.get(`${BASE_HOST}/${id}`, {
+    const response = await axios.get(`${BASE_HOST}/student/${id}`, {
         headers: authHeader()
     });
     return response
 }
 
+export const getStudentByUserId = async ( id) => {
+    console.log(authHeader())
+        const response = await axios.get(`${BASE_HOST}/users/${id}/student`, {
+            headers: authHeader()
+        });
+        return response
+    }
+
 export const postStudent = async ( body) => {
 
-    const response = await axios.post(`${BASE_HOST}`, body, {
+    const response = await axios.post(`${BASE_HOST}/student`, body, {
         headers: authHeader()
     });
     return response.data
@@ -30,7 +38,7 @@ export const postStudent = async ( body) => {
 
 export const putStudent = async ( body) => {
 
-    const response = await axios.put(`${BASE_HOST}`, body, {
+    const response = await axios.put(`${BASE_HOST}/student`, body, {
         headers: authHeader()
     });
     return response.data
